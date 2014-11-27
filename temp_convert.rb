22bbb55a524b5b01847962ca479bcd26ceb46d2c
Kenton Brede <kbrede@unomaha.edu>
2014-11-26 20:12:09 -0600

    Change error checks and move Float conversion to optparse

--- temp_convert.rb
+++ temp_convert.rb
@@ -2,51 +2,64 @@
 
 require 'optparse'
 
-# ToDo: possibly combine 2 methods into 1 
-# deduplicate Interger conversion
-
-# print help if number of options not correct
-if ARGV.length <= 1
-  ARGV << '-h'
-  ARGV[1] = '-h'
-end
-
-# print help if second arg isn't a number
-unless ARGV[1].match(/[0-9]/)
-  ARGV << '-h'
-end
-
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Converts Fahrenheit to Celsius and Celsius to Fahrenheit.\nUsage: temp_convert.rb [options]"
 
-  opts.on('-f', '--f2c number',  "Example: #{$0} -f 32") do |f|
+  opts.on('-f', '--f2c number', Float, "Example: #{$0} -f 32") do |f|
     options[:fopt] = f
   end
 
-  opts.on('-c', '--c2f number',  "Example: #{$0} -c 0") do |c|
+  opts.on('-c', '--c2f number', Float, "Example: #{$0} -c 0") do |c|
     options[:copt] = c
   end
 
-  # print help message if incorrect option
+  # if no args, print help
+  if ARGV.length == 0
+    puts opts
+    exit
+  end
+  
+  # method to check if string is numerical
+  class String
+    def is_number?
+      true if Float(self) rescue false
+    end
+  end
+
+  # check if second arg is a number
+  num = ARGV[1]
+  unless num.is_number?
+    puts opts
+    exit
+  end
+
+  # handle rest of errors
   begin opts.parse! ARGV
-  rescue OptionParser::InvalidOption => invalid_opt
-    puts invalid_opt
+  rescue OptionParser::InvalidOption => err
+    puts err
+    puts opts
+    exit 1
+  rescue OptionParser::MissingArgument => err
+    puts err
+    puts opts
+    exit 1
+  rescue OptionParser::InvalidArgument => err
+    puts err
     puts opts
     exit 1
   end
-
+ 
   opts.on('-h', '--help', 'Displays Help') do
     puts opts
     exit
   end
+  
 end.parse!
 
-# convert temperature to a float
+# set temperature variables
 ftemp = options[:fopt]
-ftemp = Float(ftemp) if ftemp != nil
 ctemp = options[:copt]
-ctemp = Float(ctemp) if ctemp != nil
 
 # convert Fahrenheit to Celsius
 def ftoc(ftemp)
